7f1b94020bf5fee1d97d60d8fa8fbb18c4a535c4
Ozan Uslan <ozanuslan@hotmail.com>
2021-12-12 20:53:27 +0300

    encode-and-decode-tinyurl

---
+++ leetcode/encode-and-decode-tinyurl.scala
@@ -0,0 +1,20 @@
+class Codec {
+    // Encodes a URL to a shortened URL.
+    def encode(longURL: String): String = {
+        longURL
+    }
+    
+    // Decodes a shortened URL to its original URL.
+    def decode(shortURL: String): String = {
+        shortURL
+    }
+}
+
+// What even is this problem???
+
+/**
+ * Your Codec object will be instantiated and called as such:
+ * var obj = new Codec()
+ * val s = obj.encode(longURL)
+ * val ans = obj.decode(s)
+ */
