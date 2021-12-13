7a8c2d50f2464549b086bcf2e62cd61d778a8392
Ozan Uslan <ozanuslan@hotmail.com>
2021-12-13 03:29:25 +0300

    fix commit script

--- commit.sh
+++ commit.sh
@@ -1,19 +1,35 @@
 #! /bin/sh
 
-# this shell script accepts two arguments:
-# 1. the name of the file with extension that will be created
-# 2. string that will be inserted into the file
+# this shell script accepts three arguments:
+# 1. name of the subfolder in which the file will be created, either "leetcode" or "hackerrank"
+# 2. the name of the file with extension that will be created
+# 3. string that will be inserted into the file
 # after the string is inserted, the file is committed
 # and the commit message is the name of the file without extension
 
-if [ $# -ne 2 ]
-then
-    echo "Usage: commit.sh <file_name> <string>"
+
+# check if the number of arguments is correct
+if [ $# -ne 3 ]; then
+    echo "Usage: $0 <folder> <filename> <string>"
     exit 1
 fi
 
-file_without_extension=$(echo "$1" | cut -f 1 -d '.')
+# check if the first argument is either "leetcode" or "hackerrank"
+if [ "$1" != "leetcode" ] && [ "$1" != "hackerrank" ]; then
+    echo "The first argument must be either \"leetcode\" or \"hackerrank\""
+    exit 1
+fi
 
-echo $2 >> $1
-git add $1
-git commit -m $file_without_extension
+# create the file
+touch "$1/$2"
+
+# insert the string into the file
+echo "$3" >> "$1/$2"
+
+file_wo_extension=$(echo "$2" | cut -f 1 -d '.')
+
+# commit the file
+git add "$1/$2"
+git commit -m "$file_wo_extension"
+
+exit 0
