afec3e64824853c32d2a119558cd727909167f72
Ozan Uslan <ozanuslan@hotmail.com>
2021-12-24 03:11:33 +0300

    find-all-numbers-disappeared-in-an-array

---
+++ leetcode/find-all-numbers-disappeared-in-an-array.scala
@@ -0,0 +1,14 @@
+object Solution {
+    def findDisappearedNumbers(nums: Array[Int]): List[Int] = {
+        var m = scala.collection.mutable.Map[Int, Int]().withDefaultValue(0)
+        for(i <- nums) {
+            m(i) += 1
+        }
+        
+       
+        (1 to nums.length).foldLeft(List[Int]())((res, n) => {
+            if (m(n) == 0) res :+ n
+            else res
+        })
+    }
+}
