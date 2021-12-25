d77f65855fd24701a8096ca2884b5a6c7c48f994
Ozan Uslan <ozanuslan@hotmail.com>
2021-12-26 01:14:31 +0300

    middle-of-the-linked-list

---
+++ leetcode/middle-of-the-linked-list.scala
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode(_x: Int = 0, _next: ListNode = null) {
+ *   var next: ListNode = _next
+ *   var x: Int = _x
+ * }
+ */
+object Solution {
+    def middleNode(head: ListNode): ListNode = {
+        var slow = head
+        var fast = head
+        
+        while(fast.next != null && fast.next.next != null) {
+            slow = slow.next
+            fast = fast.next.next
+        }
+        
+        if(fast.next == null) slow
+        else slow.next
+    }
+}
