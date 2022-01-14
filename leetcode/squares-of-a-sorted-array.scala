b3a168c7da51518143710ccb5c0cf376665c8ed5
Ozan Uslan <ozanuslan@hotmail.com>
2022-01-14 22:11:07 +0300

    squares-of-a-sorted-array

--- leetcode/squares-of-a-sorted-array.scala
+++ leetcode/squares-of-a-sorted-array.scala
@@ -3,3 +3,25 @@ object Solution {
         nums.map(Math.pow(_, 2).toInt).sorted
     }
 }
+object Solution {
+    def sortedSquares(nums: Array[Int]): Array[Int] = {
+        // O(nlogn) T O(n) S
+        // nums.map(scala.math.pow(_, 2).toInt).sorted
+        
+        // O(n) T S
+        val n = nums.length; var i = 0; var j = n - 1
+        var a = new Array[Int](n)
+        
+        for(k <- n - 1 to 0 by -1) {
+            if(nums(i).abs > nums(j).abs) {
+                a(k) = nums(i) * nums(i)
+                i += 1
+            } else {
+                a(k) = nums(j) * nums(j)
+                j -= 1
+            }
+        }
+        
+        a
+    }
+}
