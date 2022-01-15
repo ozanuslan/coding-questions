e03f3ebb148c953ae085238836fac824371833e5
Ozan Uslan <ozanuslan@hotmail.com>
2022-01-16 00:06:12 +0300

    maximum-depth-of-binary-tree

---
+++ leetcode/maximum-depth-of-binary-tree.scala
@@ -0,0 +1,14 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode(_value: Int = 0, _left: TreeNode = null, _right: TreeNode = null) {
+ *   var value: Int = _value
+ *   var left: TreeNode = _left
+ *   var right: TreeNode = _right
+ * }
+ */
+object Solution {
+    def maxDepth(root: TreeNode): Int = {
+        if(root == null) 0
+        else 1 + (maxDepth(root.left) max maxDepth(root.right))
+    }
+}
