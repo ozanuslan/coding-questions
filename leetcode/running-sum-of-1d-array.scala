a33e2294646fce31260f1b60edf9f5f45fa50ecf
Ozan Uslan <ozanuslan@hotmail.com>
2022-01-18 00:54:57 +0300

    running-sum-of-1d-array

--- leetcode/running-sum-of-1d-array.scala
+++ leetcode/running-sum-of-1d-array.scala
@@ -4,3 +4,17 @@ object Solution {
         nums.map(n => {sum+=n; sum})
     }
 }
+object Solution {
+    def runningSum(nums: Array[Int]): Array[Int] = {
+        var run = 0
+        val n = nums.length
+        var ans = new Array[Int](n)
+        
+        for(i <- 0 until n) {
+            run += nums(i)
+            ans(i) = run
+        }
+        
+        ans
+    }
+}C
