95825c5c990356ef0c0d675ab86ace9a0b7d6dc8
Ozan Uslan <ozanuslan@hotmail.com>
2022-01-18 23:23:53 +0300

    partitioning-into-minimum-number-of-deci-binary-numbers

--- leetcode/partitioning-into-minimum-number-of-deci-binary-numbers.scala
+++ leetcode/partitioning-into-minimum-number-of-deci-binary-numbers.scala
@@ -2,4 +2,8 @@ object Solution {
     def minPartitions(n: String): Int = {
         n.chars.max.getAsInt - '0'
     }
+}object Solution {
+    def minPartitions(n: String): Int = {
+        n.foldLeft(0)((acc, num) => acc max num - 48)
+    }
 }
