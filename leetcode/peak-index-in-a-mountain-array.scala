ff06780e8925a91ecd6edc2de42f5e1b7c3d02ad
Ozan Uslan <ozanuslan@hotmail.com>
2022-02-03 21:16:59 +0300

    peak-index-in-a-mountain-array

--- leetcode/peak-index-in-a-mountain-array.scala
+++ leetcode/peak-index-in-a-mountain-array.scala
@@ -17,3 +17,31 @@ object Solution {
         idx
     }
 }
+object Solution {
+    def peakIndexInMountainArray(arr: Array[Int]): Int = {
+        // O(n) time O(1) space, linear search
+//         var idx = 0
+        
+//         while(arr(idx) < arr(idx + 1)) {
+//             idx += 1
+//         }
+        
+//         idx
+        
+        // O(logn) time O(1) space, binary search
+        val n = arr.length
+        var low = 0; var high = n - 1
+        
+        while(low < high) {
+            val mid = (low + high) // 2
+            if(arr(mid - 1) < arr(mid) && arr(mid) > arr(mid + 1))
+                return mid
+            if(arr(mid - 1) < arr(mid))
+                low = mid + 1
+            else
+                high = mid - 1
+        }
+        
+        -1
+    }
+}C
