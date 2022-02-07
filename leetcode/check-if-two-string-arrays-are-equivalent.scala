f80af9b8e051189ba168bd99da5d9b5eb64e2993
Ozan Uslan <ozanuslan@hotmail.com>
2022-02-07 23:59:09 +0300

    check-if-two-string-arrays-are-equivalent

--- leetcode/check-if-two-string-arrays-are-equivalent.scala
+++ leetcode/check-if-two-string-arrays-are-equivalent.scala
@@ -5,3 +5,10 @@ object Solution {
         w1 equals w2
     }
 }
+object Solution {
+    def arrayStringsAreEqual(word1: Array[String], word2: Array[String]): Boolean = {
+        val w1 = word1.foldLeft("")((acc, s) => acc + s)
+        val w2 = word2.foldLeft("")((acc, s) => acc + s)
+        w1 equals w2
+    }
+}
