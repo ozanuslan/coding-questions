47141fbd8b9905614a3882dbc8ba1deedf0b87c0
Ozan Uslan <ozanuslan@hotmail.com>
2022-06-02 14:13:39 +0300

    sorting-the-sentence

--- leetcode/sorting-the-sentence.scala
+++ leetcode/sorting-the-sentence.scala
@@ -3,3 +3,11 @@ object Solution {
         s.split( ).map(w => (w.takeRight(1).toInt -> w.dropRight(1))).sortWith(_._1 < _._1).map(_._2).mkString( )
     }
 }
+object Solution {
+    def sortSentence(s: String): String = {
+        s.split(" ")
+        .sortWith((a,b) => a.takeRight(1) < b.takeRight(1))
+        .map(_.dropRight(1))
+        .mkString(" ")
+    }
+}
